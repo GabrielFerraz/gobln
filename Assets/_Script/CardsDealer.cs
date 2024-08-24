d63f340cbe1b7ffbffa51b6b1021926ba641e2c1
Gabriel Ferraz <gabrielmaiaferraz@gmail.com>
2024-08-24 16:24:06 -0300

    card image path on card creation

--- Assets/_Script/CardsDealer.cs
+++ Assets/_Script/CardsDealer.cs
@@ -16,23 +16,23 @@ public class CardsDealer : MonoBehaviour
         cardsDeck = new Cards[]
 #region
         {
-            new Cards(1, Cards.Naipe.Castelo),
-            new Cards(2, Cards.Naipe.Castelo),
-            new Cards(3, Cards.Naipe.Castelo),
-            new Cards(4, Cards.Naipe.Castelo),
-            new Cards(1, Cards.Naipe.Cerveja),
-            new Cards(2, Cards.Naipe.Cerveja),
-            new Cards(3, Cards.Naipe.Cerveja),
-            new Cards(4, Cards.Naipe.Cerveja),
-            new Cards(1, Cards.Naipe.Espada),
-            new Cards(2, Cards.Naipe.Espada),
-            new Cards(3, Cards.Naipe.Espada),
-            new Cards(4, Cards.Naipe.Espada),
-            new Cards(1, Cards.Naipe.Dinheiro),
-            new Cards(2, Cards.Naipe.Dinheiro),
-            new Cards(3, Cards.Naipe.Dinheiro),
-            new Cards(4, Cards.Naipe.Dinheiro),
-            new Cards(0, Cards.Naipe.Coringa),
+            new Cards(1, Cards.Naipe.Castelo, "castle-1"),
+            new Cards(2, Cards.Naipe.Castelo, "castle-2"),
+            new Cards(3, Cards.Naipe.Castelo, "castle-3"),
+            new Cards(4, Cards.Naipe.Castelo, "castle-4"),
+            new Cards(1, Cards.Naipe.Cerveja, "beer-1"),
+            new Cards(2, Cards.Naipe.Cerveja, "beer-2"),
+            new Cards(3, Cards.Naipe.Cerveja, "beer-3"),
+            new Cards(4, Cards.Naipe.Cerveja, "beer-4"),
+            new Cards(1, Cards.Naipe.Espada, "sword-1"),
+            new Cards(2, Cards.Naipe.Espada, "sword-2"),
+            new Cards(3, Cards.Naipe.Espada, "sword-3"),
+            new Cards(4, Cards.Naipe.Espada, "sword-4"),
+            new Cards(1, Cards.Naipe.Dinheiro, "coins-1"),
+            new Cards(2, Cards.Naipe.Dinheiro, "coins-2"),
+            new Cards(3, Cards.Naipe.Dinheiro, "coins-3"),
+            new Cards(4, Cards.Naipe.Dinheiro, "coins-4"),
+            new Cards(0, Cards.Naipe.Coringa, "coringa"),
         };
         #endregion
         DealCards();
@@ -61,7 +61,7 @@ public class CardsDealer : MonoBehaviour
                 goblinScript.handIsFull = true;
         }
 
-      //Printar todas as cartas de suas respectivas mãos
+      //Printar todas as cartas de suas respectivas mï¿½os
       //"Goblin 0" = player
       for (int i = 0; i < 4; i++)
           for (int j = 0; j < 4; j++)
